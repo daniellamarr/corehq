9398198eb788a77bf35c1d359c0e6964dffae455
Nifemi <deyeminifemi@gmail.com>
2018-12-19 18:19:00 +0100

    Added responsitivity

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+document.querySelector('#nav-small').addEventListener('click', () => {
+    document.querySelector('#sidenav').classList.add('active')
+    document.querySelector('#nav-small').classList.add('active')
+})
+document.querySelector('#sidenav-hide').addEventListener('click',() => {
+    document.querySelector('#sidenav').classList.remove('active')
+    document.querySelector('#nav-small').classList.remove('active') 
+})
