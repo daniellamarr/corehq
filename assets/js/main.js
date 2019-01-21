9e6d540f47ec5381c438a23e76e0d4672ac5d6dc
Nifemi <deyeminifemi@gmail.com>
2019-01-21 11:00:58 +0100

    Make sidenav hide on link click

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,19 +3,20 @@ $(".nav-menu").click(function(){
 })
 const teamCards = document.getElementById('team-cards');
 $('#card-btn-left').click(() => {
-    teamCards.scrollBy(-320,0)
+    teamCards.scrollBy(-320,0);
 })
 $('#card-btn-right').click(() => {
-    teamCards.scrollBy(320,0)
+    teamCards.scrollBy(320,0);
 })
 window.addEventListener('resize',() => {
     if(window.innerWidth > 600){
-        $('#sidenav').removeClass('active')
+        $('#sidenav').removeClass('active');
     }
 })
-
+$('#sidenav a').click(() => {
+    $('#sidenav').removeClass('active');
+})
 $('.scroll').click((e) => {
     const target = e.target.dataset.target;
-    // alert();
     window.scrollTo(0,$(`#${target}`).offset().top-10);
 })
